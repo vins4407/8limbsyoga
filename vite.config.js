5c9c9c0e7781478042fca7df2b2322fa127670f7
vins4407 <vkesherwani000@gmail.com>
2023-11-05 16:30:20 +0530

    updated .lottie configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
-  // Add this assetInclude option to handle .lottie files
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name][ext]',
-      },
-    },
-  },
   assetsInclude: ['**/*.lottie'],
 
-  // Other Vite configuration options...
-});
+  
+})
